1c6cc04e807d7b8ade042dc370d6759cbd7eb977
Tim Taylor <timthetoolmantaylor@gmail.com>
2012-09-05 14:37:55 -0700

    Initial Commit

---
+++ Kata 3 FizzBuzz/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Kata 3 FizzBuzz
+//
+//  Created by Tim Taylor on 9/5/12.
+//  Copyright (c) 2012 Tim Taylor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
